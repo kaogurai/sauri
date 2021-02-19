55e6f44f882fdddef5128f49f879163b2e9e8cda
aleclol <50505980+aleclol@users.noreply.github.com>
2021-02-19 16:03:41 -0500

    [Pick] Fix something

--- pick/pick.py
+++ pick/pick.py
@@ -7,29 +7,19 @@ from redbot.core import Config, checks, commands
 
 
 class Pick(commands.Cog):
-    """Pick a random user or a user with a specified role. For the latter, use `[p]pickrole <role>` first.
-    **Output is a user ID.**
-    I suggest using it along with [nestedcommands](https://github.com/tmercswims/tmerc-cogs) and [scheduler](https://github.com/mikeshardmind/SinbadCogs)."""
+    """Picks a random user.
+    **Output is a ping/mention.**."""
 
     __author__ = "saurichable"
     __version__ = "1.1.1"
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(
-            self, identifier=50743950055646, force_registration=True
-        )
-
-        default_guild = {"role": None}
-
-        self.config.register_guild(**default_guild)
 
     @commands.command()
     @commands.guild_only()
     @checks.mod_or_permissions(manage_roles=True)
     async def pickuser(self, ctx: commands.Context):
-        """Pick a random user. **Output is a user ID.**
-        
-        I suggest using [nestedcommands by tmerc](https://github.com/tmercswims/tmerc-cogs) (Example of usage `[p]say Congratulations <@$(pick)>! You won!`)"""
+        """Pick a random user. **Output is a user ID.**"""
         winner = random.choice(ctx.guild.members)
         await ctx.send(f"<@{winner.id}>")
